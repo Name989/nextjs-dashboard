d5e0384671fb77da2de2ecd8cef2c53f0de92ab6
HP0165 <harshit.patidar@zehntech.com>
2025-02-21 17:31:50 +0530

    solved the error in page.tsx of id

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -2,12 +2,20 @@ import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
 import { notFound } from 'next/navigation';
-export default async function Page({ params }: { params: { id: string } }) {
+
+interface PageProps {
+    params: {
+        id: string;
+    };
+}
+
+export default async function Page({ params }: PageProps) {
     const id = params.id;
     const [invoice, customers] = await Promise.all([
         fetchInvoiceById(id),
         fetchCustomers(),
     ]);
+
     if (!invoice) {
         notFound();
     }
